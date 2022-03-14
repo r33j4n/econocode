c5d6325418dbd3eb9ac99d68cacbd21633839c01
Thareejan <thareejan@gmail.com>
2022-03-14 23:26:15 +0530

    Added necessary Variables for ped() funtion.  corrected Total Revenue Function rev() Added necessary parameters

--- assignment.py
+++ assignment.py
@@ -4,15 +4,18 @@ def calc(a,b,c,d):
 
 #Function for Queriying Price Elsticity Demand
 def ped():
-	dq=float(input("Enter Change in quantity demanded :"))
-	dp=float(input("Enter Change in price :"))
+	cd=float(input("Enter current quantity demanded :"))
+	cp=float(input("Enter current price :"))
 	q=float(input("Enter Initial Quantity Demanded :"))
 	p=float(input("Enter Initial Price :"))
+	dq=cd-q
+	dp=cp-p
 	pedval=calc(dq,dp,q,p)
 	if pedval>0:
-		pedval=pedval 
+		pedval=pedval
 	else:
 		pedval*=(-1)
+	print()
 	print("------------------------------------------------------")
 	print("Price Elasticity of Demand is :",round(pedval,2))
 	if 0<pedval<1:
@@ -25,6 +28,8 @@ def ped():
 		print("Price Elasticity of demand is Elastic")
 	if dp==0:
 		print("Price Elasticity of demand is Perfectly Elastic")
+	print("------------------------------------------------------")
+	rev(cp,cd,q,p,pedval)
 
 #Function for Queriying Cross Elsticity Demand
 def xED():
@@ -68,26 +73,30 @@ def pes():
 	print("------------------------------------------------------")
 	print("Price Elasticity of Supply is :",round(pesval,2))
 
-def rev(a,b,c,d):
+def rev(a,b,c,d,e):
 	int_rev= c*d
-	new_rev= (c+a)*(b+c)
+	new_rev= a*b
 	print("Initial Revenue is: ",int_rev)
-	print("New Revenue is: ",int_rev)
-	if calc(a,b,c,d)<1:
-		print("Decrease in Revenue is: ",int_rev-new_rev)
-	elif calc(a,b,c,d)>1:
-		print("Decrease in Revenue is: ",int_rev-new_rev)
+	print("New Revenue is: ",new_rev)
+	if (e<1) and ((a-d)>0):
+		print("Increase in Total Revenue is: ",round((new_rev-int_rev),2))
+	elif (e<1) and ((a-d)<0):
+		print("Decrease in Total Revenue is: ",round((int_rev-new_rev),2))
+	if (e>1) and ((a-d)>0):
+		print("Decrease in Total Revenue is: ",round((int_rev-new_rev),2))
+	elif (e>1) and ((a-d)<0):
+		print("Increase in Total Revenue is: ",round((new_rev-int_rev),2))
+
 
 #Main Function
 while True:
 	print("Elasticity-Calculator-204216D-IS1020-Assignment-2")
 	print("What type of Elasiticity do you want to Calculate ..?")
-	print(''' 
+	print('''
 			  [1] Price Elasticity Demand
 			  [2] Cross Elasticity Demand
 			  [3] Income Elasticity of Demand
 			  [4] Price Elasticity of Supply
-			  [5] Calculate Total Revenue Status
 		''')
 	ans=int(input("Input Enter the number of the type: "))
 	if ans==(1):
@@ -101,11 +110,10 @@ while True:
 	print("------------------------------------------------------")
 	ans_=str(input("Do you want to calculate again (Y/N) ? "))
 	if (ans_=='Y') or (ans_=='y') or (ans_=='Yes') or (ans_=='yes'):
-		continue 
+		continue
 	elif (ans_=='N') or (ans_=='n') or (ans_=='no') or (ans_=='No'):
 		break
 	else:
 		print("Incorrect Input")
 		print()
 		break
-
